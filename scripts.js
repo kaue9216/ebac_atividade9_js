960abece70a7844e9cd6802987b25034b23ff5bc
kaue ogibowski <kaueogibowski@outlook.com>
2025-05-21 12:50:41 -0300

    persistir id

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const tarefas = document.getElementById("listaClientes");
 
-fetch("https://crudcrud.com/api/458831905ff44b48ba45d9fa41a0d8d7/tarefas")
+fetch("https://crudcrud.com/api/7f520d1e131b4b8685f4424cdd54e106/tarefas")
 .then(resposta => resposta.json())
 .then((listaDeTarefas) => {
   listaDeTarefas.forEach(tarefa => {
@@ -12,27 +12,28 @@ fetch("https://crudcrud.com/api/458831905ff44b48ba45d9fa41a0d8d7/tarefas")
 
 document.getElementById("add").addEventListener("click", () => {
   const descricao = document.getElementById("tarefa").value;
-  fetch("https://crudcrud.com/api/458831905ff44b48ba45d9fa41a0d8d7/tarefas", {
+  fetch("https://crudcrud.com/api/7f520d1e131b4b8685f4424cdd54e106/tarefas", {
     method: "POST",
     headers:{
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({descricao: descricao})
+    body: JSON.stringify({descricao: descricao, _id: _id})
   })
   .then(resposta => resposta.json())
   .then((tarefa) => {
     const item = document.createElement("li");
-    item.innerHTML = `${tarefa.descricao} <button id="delete" onclick="remove(${tarefa._id})">X</button>`;
+    item.innerHTML = `${tarefa.descricao} ${tarefa._id}<button id="delete">X</button>`;
     tarefas.appendChild(item);
   })
 })
 
-function remove(id) {
+document.getElementById("delete")
+document.addEventListener("click", () => {
   console.log(document.getElementById("delete"))
-  fetch(`https://crudcrud.com/api/458831905ff44b48ba45d9fa41a0d8d7/tarefas/${id}`,{
+  fetch(`https://crudcrud.com/api/7f520d1e131b4b8685f4424cdd54e106/tarefas`,{
     method: "DELETE",
     headers:{
       "Content-Type": "application/json"
     },
   })
-}
+})
